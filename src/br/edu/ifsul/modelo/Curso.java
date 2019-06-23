4ba134e46cdca1b09febb19ae21f7c458e3932ca
guilherme <guilherme@Gehring>
2019-06-23 12:23:09 -0300

    Correção em alguns métodos em Curso.java e criação de Usuario.java

--- src/br/edu/ifsul/modelo/Curso.java
+++ src/br/edu/ifsul/modelo/Curso.java
@@ -34,46 +34,46 @@ import org.hibernate.validator.constraints.NotBlank;
  */
 @Entity
 @Table(name = "curso")
-public class Curso implements Serializable{
-    
+public class Curso implements Serializable {
+
     @Id
     @SequenceGenerator(name = "seq_curso", sequenceName = "seq_curso_id", allocationSize = 1)
     @GeneratedValue(generator = "seq_curso", strategy = GenerationType.SEQUENCE)
     private Integer id;
-    
+
     @NotNull(message = "O nome não pode ser nulo")
     @NotBlank(message = "O nome não pode ficar em branco")
     @Length(max = 100, message = "O nome não pode ter mais que {max} caracteres")
     @Column(name = "nome", length = 100, nullable = false)
     private String nome;
-    
+
     @NotNull(message = "A sigla não pode ser nula")
     @NotBlank(message = "A sigla não pode ficar em branco")
     @Length(max = 2, message = "A sigla não pode ter mais que {max} caracteres")
     @Column(name = "sigla", length = 2, nullable = false)
     private String sigla;
-    
+
     @NotNull(message = "A descrição não pode ser nula")
-    @NotBlank(message = "A descrição não pode ficar em branco")    
+    @NotBlank(message = "A descrição não pode ficar em branco")
     @Column(name = "descricao", nullable = false)
     private String descricao;
-    
+
     private Boolean ativo;
-    
+
     @Temporal(TemporalType.DATE)
     @NotNull(message = "A data de início deve ser informada")
     @Column(name = "inicio_atividades", nullable = false)
     private Calendar inicioAtividades;
-    
+
     @NotNull(message = "A instituição deve ser informada")
     @ManyToOne
     @JoinColumn(name = "instituicao", referencedColumnName = "id", nullable = false,
             foreignKey = @ForeignKey(name = "fk_instituicao_id"))
     private Instituicao instituicao;
-    
+
     @OneToMany(mappedBy = "curso", cascade = CascadeType.ALL,
-        orphanRemoval = true, fetch = FetchType.LAZY)
-    private List<Disciplina> disciplinas;//composição, ou seja, a disciplina nao vive sem o curos. Disciplina por sua vez, referencia Curso. É bidirecional pois, não tem sentido       
+            orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Disciplina> disciplinas;//composição, ou seja, a disciplina nao vive sem o curso. Disciplina por sua vez, referencia Curso. É bidirecional pois, não tem sentido
 
     public Curso() {
         this.ativo = true;
@@ -134,13 +134,22 @@ public class Curso implements Serializable{
     public void setInstituicao(Instituicao instituicao) {
         this.instituicao = instituicao;
     }
-    
+
     public List<Disciplina> getDisciplinas() {
         return disciplinas;
     }
 
     public void setDisciplinas(List<Disciplina> disciplinas) {
         this.disciplinas = disciplinas;
+    }
+
+    public void adicionarDisciplina(Disciplina obj) {
+        obj.setCurso(this);
+        this.getDisciplinas().add(obj);
+    }
+
+    public void removerDisciplina(int index) {
+        this.getDisciplinas().remove(index);
     }
 
     @Override
@@ -166,6 +175,6 @@ public class Curso implements Serializable{
             return false;
         }
         return true;
-    }           
-    
+    }
+
 }
